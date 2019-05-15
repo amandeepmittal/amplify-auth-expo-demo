5cfbe3ef39e26f135d01d35c59b4cf0caf352519
Aman Mittal <amandeepmittal@live.com>
2019-05-15 18:10:19 +0530

    add HOC withAuthenticator and test default auth flow

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
-
-// --- This is the part to add
-
 import Amplify from "aws-amplify"
 import config from "./aws-exports"
+import { withAuthenticator } from "aws-amplify-react-native"
 
 Amplify.configure(config)
 
-// ---
-
-export default class App extends React.Component {
+class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
@@ -28,3 +24,5 @@ const styles = StyleSheet.create({
 		justifyContent: "center"
 	}
 })
+
+export default withAuthenticator(App, true)
