cae2ea4ed8503ada076d793c699fa7e90f53efcb
Aman Mittal <amandeepmittal@live.com>
2019-05-15 16:53:54 +0530

    amplify: init, integrate, verify

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+// --- This is the part to add
+
+import Amplify from "aws-amplify"
+import config from "./aws-exports"
+
+Amplify.configure(config)
+
+// ---
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>React Native + Amplify = 💛</Text>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
+})
